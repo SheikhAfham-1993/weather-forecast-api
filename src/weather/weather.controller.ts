30ff17acdefa2f00875462c64f4efc3ac72b7d1d
SheikhAfham-1993 <56600934+SheikhAfham-1993@users.noreply.github.com>
2023-11-04 14:16:49 +0100

    Added weather stack api connectivity

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { WeatherService } from './weather.service';
+
 
 @Controller("weather")
 export class WeatherController {
   constructor(private weatherService: WeatherService) {}
 
   @Get('/getWeather')
-  getWeatherData() {
-    return this.weatherService.getWeatherData();
+  getWeatherData(@Req() req) {
+    return this.weatherService.getWeatherData(req.body.city);
+  }
+
+  @Get('/getHistoricalWeather')
+  getHistoricalWeather(@Req() req) {
+    return this.weatherService.getHistoricalWeather();
   }
 }
--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import axios from "axios";
 
 @Injectable({})
 export class WeatherService {
-    getWeatherData() {
+    getWeatherData(city: string) {
+        return axios.get(`${process.env.API_URL}/current?access_key=${process.env.API_KEY}&query=${city}`)
+        .then((response) => {return response.data});
+    }
+
+    getHistoricalWeather() {
         return "Hello World!";
     }
 }
